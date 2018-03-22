131b05d668efc379cbcf498be3048b84638030be
Niels <Niels97vet@hotmail.com>
2018-03-22 08:54:07 +0100

    added Ui sprites and placed on right place with mouse input.

--- ts/Events/Interface/UiSprites.ts
+++ ts/Events/Interface/UiSprites.ts
@@ -12,16 +12,32 @@ export default class UiManager
         this.game.load.image('Attack_Button', 'assets/sprites/ui/Ui_Attack.png')
         this.game.load.image('Defend_Button', 'assets/sprites/ui/Ui_Defend.png')
         this.game.load.image('Passive_Button', 'assets/sprites/ui/Ui_Passive.png')
+        
     }
     
     public create():void {
     
-        this.game.add.sprite(100,100,'Attack_Button');
-        this.game.add.sprite(100,100,'Defend_Button');
-        this.game.add.sprite(100,100,'Passive_Button');
-        //  This simply creates a sprite using the mushroom image we loaded above and positions it at 200 x 200
-        //var test = game.add.sprite(200, 200, 'mushroom');
-    
+        var UiLayer = this.game.add.group();
+
+        var AttackImage = UiLayer.create(100,1100,'Attack_Button');
+        var DefendImage = UiLayer.create(260,1000,'Defend_Button');
+        var PassiveImage = UiLayer.create(420,1100,'Passive_Button');
+
+        //AttackImage.anchor.set(0.5);
+        //DefendImage.anchor.set(0.5);
+        //PassiveImage.anchor.set(0.5);
+
+        AttackImage.inputEnabled = true;
+        DefendImage.inputEnabled = true;
+        PassiveImage.inputEnabled = true;
+
+        AttackImage.events.onInputDown.add(this.Help,this);
+        DefendImage.events.onInputDown.add(this.Help,this);
+        PassiveImage.events.onInputDown.add(this.Help,this);
+    }
+
+    public Help():void{
+        console.log('help')
     }
 
 }
