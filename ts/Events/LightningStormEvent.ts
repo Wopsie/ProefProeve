bf55fecf1e4db63eb77896542d2864257afd50ad
koedijk <17798@ma-web.nl>
2018-03-29 09:43:13 +0200

    forgotten script

--- ts/Events/LightningStormEvent.ts
+++ ts/Events/LightningStormEvent.ts
@@ -1,5 +1,6 @@
 import iEvent from "./Interface/iEvent";
 import { EventTypes } from "./EventGenerator";
+import Timer from "../Timer/Timer";
 
 export default class LightningStormEvent implements iEvent{
     public eventType : EventTypes;
@@ -7,21 +8,26 @@ export default class LightningStormEvent implements iEvent{
     public npcAssets : Phaser.Image;
     public completionSignal : Phaser.Signal = new Phaser.Signal();
     public startSignal : Phaser.Signal = new Phaser.Signal();
+    private timer : Timer;
 
-    constructor(name : string = "Lightning strike"){
+    constructor(game : Phaser.Game,timer : Timer,name : string = "Lightning strike"){
         this.eventName = name;
         this.eventType = EventTypes.lightningStorm;
+        this.timer = timer;
         console.log("LIGHTNING STRIKE");
     }
 
     StartEvent():void{
         this.startSignal.dispatch(true);
+        this.timer.Create();
+        this.timer.PlayTimer(2500);
         console.log(this.eventName + " STARTING NOW");
         
     }
 
     AgressiveAction():void{
         console.log(this.eventName + " agressive action");
+        this.timer.StopTimer();
         this.Success();
     }
 
@@ -32,6 +38,7 @@ export default class LightningStormEvent implements iEvent{
 
     PassiveAction():void{
         console.log(this.eventName + " avoid action");
+        this.timer.StopTimer();
         this.Success();
     }
 
